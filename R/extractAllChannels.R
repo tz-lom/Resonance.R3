ba197637337ef05341446001021f1959f3f92900
ksv <ksv>
2017-04-06 21:22:49 +0300

    Fix reading of empty files

--- R/extractAllChannels.R
+++ R/extractAllChannels.R
@@ -2,13 +2,18 @@ extractAllChannels <- function(file){
   data <- readStructurized(normalizePath(file, mustWork = T))
   
   ret <- lapply(data$streams, function(si){
-    do.call(
-      merge,
-      Filter(
-        function(b) identical(SI(b), si) ,
-        data$blocks
-      )
+    flt <- Filter(
+      function(b) identical(SI(b), si) ,
+      data$blocks
     )
+    if(length(flt)>0){
+      do.call(
+        merge,
+        flt
+      )
+    } else {
+      makeEmpty(si)
+    }
   })
   
   names(ret) <- sapply(data$streams, '[[', 'name')
